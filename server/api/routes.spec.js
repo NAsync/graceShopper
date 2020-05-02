3dcca151a12bee4dffbb6279a73cc5df499593e4
Josh Avon <joshuacalebavon@gmail.com>
2020-05-02 18:32:46 -0400

    updated routes specs and included user specs

--- server/api/routes.spec.js
+++ server/api/routes.spec.js
@@ -9,15 +9,29 @@ const Review = db.model('review')
 const User = db.model('user')
 
 describe('Department, Products, Reviews, and Brand routes', () => {
+  let user1
+  let user2
+  let brand1
+  let brand2
+  let department1
+  let department2
+  let product1
+  let product2
+  let review1
+  let review2
+
   beforeEach(async () => {
     await db.sync({force: true})
-    await User.create({email: 'cody@email.com', password: '123'})
-    await User.create({email: 'murphy@email.com', password: '123'})
-    await Brand.create({name: '4M'})
-    await Brand.create({name: 'Whole Foods'})
-    await Department.create({name: 'health'})
-    await Department.create({name: 'grocery'})
-    await Product.create({
+    user1 = await User.create({email: 'cody@email.com', password: '123'})
+    user2 = await User.create({
+      email: 'murphy@email.com',
+      password: '123'
+    })
+    brand1 = await Brand.create({name: '4M'})
+    brand2 = await Brand.create({name: 'Whole Foods'})
+    department1 = await Department.create({name: 'health'})
+    department2 = await Department.create({name: 'grocery'})
+    product1 = await Product.create({
       name: 'Nose Cover Scarf',
       unit: '1 PC',
       description: '100% Cotton; Face Mask for Dust & Sun Protection',
@@ -25,11 +39,11 @@ describe('Department, Products, Reviews, and Brand routes', () => {
       imageURL: 'https://picsum.photos/250',
       inventoryQTY: 25,
       bestSeller: false,
-      departmentId: 1,
-      brandId: 1
+      departmentId: department1.id,
+      brandId: brand1.id
     })
 
-    await Product.create({
+    product2 = await Product.create({
       name: 'Cotton Face Mask',
       unit: '3 PC',
       description:
@@ -38,20 +52,20 @@ describe('Department, Products, Reviews, and Brand routes', () => {
       imageURL: 'https://picsum.photos/250',
       inventoryQTY: 80,
       bestSeller: false,
-      departmentId: 2,
-      brandId: 2
+      departmentId: department2.id,
+      brandId: brand2.id
     })
-    await Review.create({
+    review1 = await Review.create({
       rating: 5,
       description: 'Awesome! I can still see.',
-      userId: 1,
-      productId: 2
+      userId: user1.id,
+      productId: product2.id
     })
-    await Review.create({
+    review2 = await Review.create({
       rating: 4,
       description: 'Nice! I can still breath.',
-      userId: 2,
-      productId: 1
+      userId: user2.id,
+      productId: product1.id
     })
   })
 
@@ -62,44 +76,57 @@ describe('Department, Products, Reviews, and Brand routes', () => {
         .expect(200)
 
       expect(res.body).to.be.an('array')
-      expect(res.body[0].name).to.be.equal('Nose Cover Scarf')
-      expect(res.body[1].price).to.be.equal(30)
-    })
-    it('POST /api/products', async () => {
-      const res = await request(app)
-        .post('/api/products/')
-        .send({
-          name: 'Safety Eyewear',
-          unit: '2 PC',
-          description:
-            'KEEP YOUR EYES PERFECTLY PROTECTED; REMAIN COMFORTABLE AND STYLISH AT ALL TIMES',
-          price: 20,
-          imageURL: 'https://picsum.photos/250',
-          inventoryQTY: 30,
-          bestSeller: false,
-          departmentId: 2,
-          brandId: 1
-        })
-        .expect(201)
-
-      expect(res.body).to.be.an('object')
-      expect(res.body.name).to.be.equal('Safety Eyewear')
-      expect(res.body.price).to.be.equal(20)
+      expect(res.body[0].name).to.be.equal(product1.name)
+      expect(res.body[1].price).to.be.equal(product2.price)
     })
 
-    it('UPDATES /api/products/:id', async () => {
+    it('GET /api/products/:id', async () => {
       const res = await request(app)
-        .put('/api/products/1')
-        .send({
-          price: 13,
-          inventoryQTY: 5
-        })
+        .get('/api/products/1')
         .expect(200)
 
       expect(res.body).to.be.an('object')
-      expect(res.body.name).to.be.equal('Nose Cover Scarf')
-      expect(res.body.price).to.be.equal(13)
-      expect(res.body.inventoryQTY).to.be.equal(5)
+      expect(res.body.name).to.be.equal(product1.name)
+      expect(res.body.price).to.be.equal(product1.price)
+      expect(res.body.reviews[0].rating).to.be.equal(review2.rating)
+    })
+    it('POST /api/products', async () => {
+      const product3 = {
+        name: 'Safety Eyewear',
+        unit: '2 PC',
+        description:
+          'KEEP YOUR EYES PERFECTLY PROTECTED; REMAIN COMFORTABLE AND STYLISH AT ALL TIMES',
+        price: 20,
+        imageURL: 'https://picsum.photos/250',
+        inventoryQTY: 30,
+        bestSeller: false,
+        departmentId: department2.id,
+        brandId: brand1.id
+      }
+      const res = await request(app)
+        .post('/api/products/')
+        .send(product3)
+        .expect(201)
+
+      expect(res.body).to.be.an('object')
+      expect(res.body.name).to.be.equal(product3.name)
+      expect(res.body.price).to.be.equal(product3.price)
+    })
+
+    it('UPDATES /api/products/:id', async () => {
+      const myUpdate = {
+        price: 13,
+        inventoryQTY: 5
+      }
+      const res = await request(app)
+        .put('/api/products/1')
+        .send(myUpdate)
+        .expect(200)
+
+      expect(res.body).to.be.an('object')
+      expect(res.body.name).to.be.equal(product1.name)
+      expect(res.body.price).to.be.equal(myUpdate.price)
+      expect(res.body.inventoryQTY).to.be.equal(myUpdate.inventoryQTY)
     })
 
     it('DELETES /api/products/:id', async () => {
@@ -111,7 +138,7 @@ describe('Department, Products, Reviews, and Brand routes', () => {
         .expect(200)
 
       expect(res.body).to.be.an('array')
-      expect(res.body[0].name).to.be.equal('Cotton Face Mask')
+      expect(res.body[0].name).to.be.equal(product2.name)
       expect(res.body.length).to.be.equal(1)
     })
   })
@@ -125,31 +152,43 @@ describe('Department, Products, Reviews, and Brand routes', () => {
         .expect(200)
 
       expect(res.body).to.be.an('array')
-      expect(res.body[0].name).to.be.equal('health')
+      expect(res.body[0].name).to.be.equal(department1.name)
       expect(res.body.length).to.be.equal(2)
     })
-    it('POST /api/departments', async () => {
-      const res = await request(app)
-        .post('/api/departments/')
-        .send({
-          name: 'toilet paper'
-        })
-        .expect(201)
 
-      expect(res.body).to.be.an('object')
-      expect(res.body.name).to.be.equal('toilet paper')
-    })
-
-    it('UPDATES /api/departments/:id', async () => {
+    it('GET /api/departments/:id', async () => {
       const res = await request(app)
-        .put('/api/departments/1')
-        .send({
-          name: 'new-health'
-        })
+        .get('/api/departments/1')
         .expect(200)
 
       expect(res.body).to.be.an('object')
-      expect(res.body.name).to.be.equal('new-health')
+      expect(res.body.name).to.be.equal(department1.name)
+      expect(res.body.products[0].name).to.be.equal(product1.name)
+    })
+    it('POST /api/departments', async () => {
+      const department3 = {
+        name: 'toilet paper'
+      }
+      const res = await request(app)
+        .post('/api/departments/')
+        .send(department3)
+        .expect(201)
+
+      expect(res.body).to.be.an('object')
+      expect(res.body.name).to.be.equal(department3.name)
+    })
+
+    it('UPDATES /api/departments/:id', async () => {
+      const departmentUpdate = {
+        name: 'new-health'
+      }
+      const res = await request(app)
+        .put('/api/departments/1')
+        .send(departmentUpdate)
+        .expect(200)
+
+      expect(res.body).to.be.an('object')
+      expect(res.body.name).to.be.equal(departmentUpdate.name)
     })
 
     it('DELETES /api/departments/:id', async () => {
@@ -161,7 +200,7 @@ describe('Department, Products, Reviews, and Brand routes', () => {
         .expect(200)
 
       expect(res.body).to.be.an('array')
-      expect(res.body[0].name).to.be.equal('grocery')
+      expect(res.body[0].name).to.be.equal(department2.name)
       expect(res.body.length).to.be.equal(1)
     })
   })
@@ -174,31 +213,42 @@ describe('Department, Products, Reviews, and Brand routes', () => {
         .expect(200)
 
       expect(res.body).to.be.an('array')
-      expect(res.body[0].name).to.be.equal('4M')
+      expect(res.body[0].name).to.be.equal(brand1.name)
       expect(res.body.length).to.be.equal(2)
     })
-    it('POST /api/brands', async () => {
+    it('GET /api/brands/:id', async () => {
       const res = await request(app)
-        .post('/api/brands/')
-        .send({
-          name: 'charmin'
-        })
-        .expect(201)
-
-      expect(res.body).to.be.an('object')
-      expect(res.body.name).to.be.equal('charmin')
-    })
-
-    it('UPDATES /api/brands/:id', async () => {
-      const res = await request(app)
-        .put('/api/brands/1')
-        .send({
-          name: '40M'
-        })
+        .get('/api/brands/1')
         .expect(200)
 
       expect(res.body).to.be.an('object')
-      expect(res.body.name).to.be.equal('40M')
+      expect(res.body.name).to.be.equal(brand1.name)
+      expect(res.body.products[0].name).to.be.equal(product1.name)
+    })
+    it('POST /api/brands', async () => {
+      const brand3 = {
+        name: 'charmin'
+      }
+      const res = await request(app)
+        .post('/api/brands/')
+        .send(brand3)
+        .expect(201)
+
+      expect(res.body).to.be.an('object')
+      expect(res.body.name).to.be.equal(brand3.name)
+    })
+
+    it('UPDATES /api/brands/:id', async () => {
+      const brandUpdate = {
+        name: '40M'
+      }
+      const res = await request(app)
+        .put('/api/brands/1')
+        .send(brandUpdate)
+        .expect(200)
+
+      expect(res.body).to.be.an('object')
+      expect(res.body.name).to.be.equal(brandUpdate.name)
     })
 
     it('DELETES /api/brands/:id', async () => {
@@ -210,7 +260,7 @@ describe('Department, Products, Reviews, and Brand routes', () => {
         .expect(200)
 
       expect(res.body).to.be.an('array')
-      expect(res.body[0].name).to.be.equal('Whole Foods')
+      expect(res.body[0].name).to.be.equal(brand2.name)
       expect(res.body.length).to.be.equal(1)
     })
   }) // end describe('/api/brands/')
@@ -222,36 +272,38 @@ describe('Department, Products, Reviews, and Brand routes', () => {
         .expect(200)
 
       expect(res.body).to.be.an('array')
-      expect(res.body[0].description).to.be.equal('Awesome! I can still see.')
+      expect(res.body[0].description).to.be.equal(review1.description)
       expect(res.body.length).to.be.equal(2)
     })
     it('POST /api/reviews', async () => {
+      const review3 = {
+        id: 3,
+        rating: 4,
+        description: 'Not Bad.',
+        userId: user1.id,
+        productId: product1.id
+      }
       const res = await request(app)
         .post('/api/reviews/')
-        .send({
-          id: 3,
-          rating: 4,
-          description: 'Not Bad.',
-          userId: 1,
-          productId: 1
-        })
+        .send(review3)
         .expect(201)
 
       expect(res.body).to.be.an('object')
-      expect(res.body.rating).to.be.equal(4)
-      expect(res.body.description).to.be.equal('Not Bad.')
+      expect(res.body.rating).to.be.equal(review3.rating)
+      expect(res.body.description).to.be.equal(review3.description)
     })
 
     it('UPDATES /api/reviews/:id', async () => {
+      const reviewUpdate = {
+        description: 'Not as good as I thought'
+      }
       const res = await request(app)
         .put('/api/reviews/1')
-        .send({
-          description: 'Not as good as I thought'
-        })
+        .send(reviewUpdate)
         .expect(200)
 
       expect(res.body).to.be.an('object')
-      expect(res.body.description).to.be.equal('Not as good as I thought')
+      expect(res.body.description).to.be.equal(reviewUpdate.description)
     })
 
     it('DELETES /api/reviews/:id', async () => {
@@ -263,8 +315,27 @@ describe('Department, Products, Reviews, and Brand routes', () => {
         .expect(200)
 
       expect(res.body).to.be.an('array')
-      expect(res.body[0].description).to.be.equal('Nice! I can still breath.')
+      expect(res.body[0].description).to.be.equal(review2.description)
       expect(res.body.length).to.be.equal(1)
     })
   }) // end describe('/api/brands/')
+  describe('/api/users/', () => {
+    it('GET /api/users', async () => {
+      const res = await request(app)
+        .get('/api/users')
+        .expect(200)
+
+      expect(res.body).to.be.an('array')
+      expect(res.body[0].email).to.be.equal(user1.email)
+    })
+    it('GET /api/users/:id', async () => {
+      const res = await request(app)
+        .get('/api/users/1')
+        .expect(200)
+
+      expect(res.body).to.be.an('object')
+      expect(res.body.email).to.be.equal(user1.email)
+      expect(res.body.reviews[0].description).to.be.equal(review1.description)
+    })
+  }) // end describe('/api/users')
 }) // end describe('routes')
