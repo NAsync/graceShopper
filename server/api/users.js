7e77745dd892a7d1398d4d4e42f7dc3acd217e05
Josh Avon <joshuacalebavon@gmail.com>
2020-05-02 13:45:36 -0400

    updated Users API

--- server/api/users.js
+++ server/api/users.js
@@ -4,7 +4,9 @@ module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
-    const users = await User.findAll()
+    const users = await User.findAll({
+      attributes: ['id', 'email']
+    })
     res.json(users)
   } catch (err) {
     next(err)
