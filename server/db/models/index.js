0008ee431f8e1276b8d77443d5250585c2d03cc8
Josh Avon <joshuacalebavon@gmail.com>
2020-05-02 20:27:09 -0400

    nevermind, needs has many to pass tests

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -8,9 +8,13 @@ const {CreditCard, Merchant} = require('./creditCards')
 CreditCard.belongsTo(User)
 CreditCard.belongsTo(Merchant)
 Product.belongsTo(Department)
+Department.hasMany(Product)
 Product.belongsTo(Brand)
+Brand.hasMany(Product)
 Review.belongsTo(User)
+User.hasMany(Review)
 Review.belongsTo(Product)
+Product.hasMany(Review)
 
 module.exports = {
   User,
