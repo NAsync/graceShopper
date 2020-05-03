7973e493c18a0a0b00cfa57736eff2ab38d0dcdd
Josh Avon <joshuacalebavon@gmail.com>
2020-05-02 20:23:38 -0400

    deleted has many from db/models/index.js

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,20 +1,16 @@
-const Brand = require('./brand')
-const {CreditCard, Merchant} = require('./creditCards')
-const Department = require('./department')
+const User = require('./user')
 const Product = require('./product')
 const Review = require('./review')
-const User = require('./user')
+const Department = require('./department')
+const Brand = require('./brand')
+const {CreditCard, Merchant} = require('./creditCards')
 
 CreditCard.belongsTo(User)
 CreditCard.belongsTo(Merchant)
 Product.belongsTo(Department)
-Department.hasMany(Product)
 Product.belongsTo(Brand)
-Brand.hasMany(Product)
 Review.belongsTo(User)
-User.hasMany(Review)
 Review.belongsTo(Product)
-Product.hasMany(Review)
 
 module.exports = {
   User,
