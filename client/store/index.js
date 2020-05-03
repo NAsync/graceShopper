301a26a58730cacb74c677aaecefe70d95e32033
Andres Callanaupa <avc278@nyu.edu>
2020-05-02 22:38:16 -0400

    implemented reducers for products. need to add tests

--- client/store/index.js
+++ client/store/index.js
@@ -2,9 +2,14 @@ import {createStore, combineReducers, applyMiddleware} from 'redux'
 import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
+import {productReducer, productsReducer} from './products/reducers'
 import user from './user'
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({
+  productReducer,
+  productsReducer,
+  user
+})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
---
+++ client/store/products/actions.js
@@ -0,0 +1,83 @@
+import axios from 'axios'
+import {
+  CREATE_PRODUCT,
+  DELETE_PRODUCT,
+  READ_PRODUCT,
+  READ_PRODUCTS,
+  UPDATE_PRODUCT
+} from './action_types'
+
+const _createProduct = product => {
+  return {
+    type: CREATE_PRODUCT,
+    product
+  }
+}
+
+const _deleteProduct = product => {
+  return {
+    type: DELETE_PRODUCT,
+    product
+  }
+}
+
+const _readProduct = product => {
+  return {
+    type: READ_PRODUCT,
+    product
+  }
+}
+
+const _readProducts = products => {
+  return {
+    type: READ_PRODUCTS,
+    products
+  }
+}
+
+const _updateProduct = product => {
+  return {
+    type: UPDATE_PRODUCT,
+    product
+  }
+}
+
+const createProduct = product => {
+  return async dispatch => {
+    const createdProduct = (await axios.post('/api/products', product)).data
+    dispatch(_createProduct(createdProduct))
+  }
+}
+
+const deleteProduct = product => {
+  return async dispatch => {
+    await axios.delete(`/api/products/${product.id}`)
+    dispatch(_deleteProduct(product))
+  }
+}
+
+const readProduct = product => {
+  return async dispatch => {
+    const _product = (await axios.get(`/api/products/${product.id}`)).data
+    dispatch(_readProduct(_product))
+  }
+}
+
+const readProducts = () => {
+  return async dispatch => {
+    const products = (await axios.get('/api/products')).data
+    dispatch(_readProducts(products))
+  }
+}
+
+const updateProduct = product => {
+  return async dispatch => {
+    const updatedProduct = (await axios.put(
+      `/api/products/${product.id}`,
+      product
+    )).data
+    dispatch(_updateProduct(updatedProduct))
+  }
+}
+
+export {createProduct, deleteProduct, readProduct, readProducts, updateProduct}
