b850fb04ec84b9ec7434b0c2d83e451e2631cea7
RobertPeng <jenpopeng@gmail.com>
2020-05-08 18:47:09 -0400

    reply review requests

--- server/db/models/product.js
+++ server/db/models/product.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
-const {STRING, BOOLEAN, INTEGER, FLOAT, JSON} = Sequelize
+const {STRING, BOOLEAN, INTEGER, FLOAT} = Sequelize
 
 const Product = db.define('product', {
   name: {
