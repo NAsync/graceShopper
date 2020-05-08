18515d5c2d48be37f95e6513f79c3f69be31b451
Andres Callanaupa <avc278@nyu.edu>
2020-05-08 18:01:12 -0400

    wip

--- client/components/adminProductSingle.js
+++ client/components/adminProductSingle.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 
+const nonEditableFields = ['brandId', 'createdAt', 'id', 'reviews', 'updatedAt']
+
 class AdminProductSingle extends Component {
   constructor() {
     super()
@@ -9,18 +11,61 @@ class AdminProductSingle extends Component {
   render() {
     const {product} = this.props
     const entries = Object.entries(product)
+    const displayEntries = []
+    entries.forEach(entry => {
+      let [k, v] = entry
+      if (k === 'brand') {
+        displayEntries.push([k, v.name])
+        // } else if (k === 'reviews' || k === 'brandId') {
+      } else {
+        displayEntries.push([k, v])
+      }
+    })
     return (
-      <ul>
-        {entries.map((entry, idx) => {
-          return (
-            <li key={idx}>
-              {`${entry[0]}`}: {`${entry[1]}`}
-            </li>
-          )
-        })}
-      </ul>
+      <div>
+        <table className="admin-table">
+          <thead>
+            <tr>
+              <th>Property</th>
+              <th>Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            {displayEntries.map((entry, idx) => {
+              let [k, v] = entry
+              return (
+                <tr
+                  key={idx}
+                  className={
+                    nonEditableFields.includes(k)
+                      ? 'admin-non-edit'
+                      : 'admin-edit'
+                  }
+                >
+                  <td>{`${k}`}</td>
+                  <td>{`${v}`}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+        <button>Submit</button>
+      </div>
     )
   }
+}
+
+{
+  /* <select onChange={ ev => update(id, ev.target.value)} >
+    <option key='null' value='null'>-- Add Student --</option>
+    {notSchoolStudents.map( student => {
+        return (
+            <option key={student.id} value={student.id}>
+                {student.firstName} {student.lastName}
+            </option>
+        )
+    })}
+</select> */
 }
 
 const mapStateToProps = ({product}) => {
