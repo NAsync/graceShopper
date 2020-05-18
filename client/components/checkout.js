acada664f85cd29b51b329c2d27d589ceea8ee4f
RobertPeng <jenpopeng@gmail.com>
2020-05-18 00:50:57 -0400

    add success component after paid

--- client/components/checkout.js
+++ client/components/checkout.js
@@ -2,13 +2,14 @@ import React, {Component} from 'react'
 import StripeCheckoutButton from './stripeCheckoutButton'
 import {connect} from 'react-redux'
 import {checkoutCart} from '../store/cart/actions'
+import {Redirect} from 'react-router-dom'
 
 //todo list:
 //1.input shipping address and bring it to stripe payment
 //2.add tax depends on NY or NJ?
 //3.show product items or not?
 //4.remove chart items when successfully paid, put request -----ok
-//5.response for successfully charge - a successful route/redirect or just a message?
+//5.response for successfully charge - a successful route/redirect or just a message? ------ok
 //6.deduct inventory qty (also show out of stock in products)
 //7.response for failed charge - a message, redirect?
 //8.whatelse data to bring in stripe payment
@@ -17,7 +18,11 @@ import {checkoutCart} from '../store/cart/actions'
 class Checkout extends Component {
   constructor() {
     super()
-    this.sumAmount = this.sumAmount.bind()
+    this.state = {
+      success: false
+    }
+    this.sumAmount = this.sumAmount.bind(this)
+    this.redirectSuccess = this.redirectSuccess.bind(this)
   }
 
   sumAmount(orderProducts) {
@@ -31,15 +36,27 @@ class Checkout extends Component {
     }
   }
 
+  redirectSuccess() {
+    this.setState({success: true})
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.success !== prevState.success) {
+      this.setState({success: false})
+    }
+  }
+
   render() {
-    const {sumAmount} = this
+    const {sumAmount, redirectSuccess} = this
     const totalAmount = sumAmount(this.props.cart.orderProducts)
     const userId = this.props.cart.userId
     const cartCheckout = this.props.cartCheckout
     const order = {
       totalAmount: totalAmount
     }
-
+    if (this.state.success) {
+      return <Redirect push to="/success" />
+    }
     return (
       <div className="containerCheckout">
         <div className="totalAmtCheckout">{`Your Order Total $${totalAmount}`}</div>
@@ -47,6 +64,7 @@ class Checkout extends Component {
           order={order}
           cartCheckout={cartCheckout}
           userId={userId}
+          redirectSuccess={redirectSuccess}
         />
       </div>
     )
